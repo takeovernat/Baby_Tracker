b4255854bb53dde372ca11c0ec7e7049559370a3
Kaleab Alemu <kaleabunited@gmail.com>
2021-10-12 11:57:17 -0500

    Added new screen and modified css

---
+++ Frontend/baby_tracker_react/styles/button.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native'
+
+export default function FlatButton({text, onPress}) {
+    return(
+        <TouchableOpacity onPress={onPress}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}> {text} </Text>
+            </View>
+        </TouchableOpacity>
+
+    )}
+
+    const styles = StyleSheet.create({
+        button: {
+            borderRadius: 8,
+            paddingHorizontal: 10,
+            paddingVertical: 10,
+            backgroundColor: '#FFDEFA'
+        },
+        buttonText: {
+            color: 'black',
+            fontSize: 18,
+            textAlign: 'center', 
+            fontFamily: 'Noteworthy',
+            fontWeight: 'bold',
+        }
+      });

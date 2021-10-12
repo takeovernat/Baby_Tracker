7f2dae58d7b06964e863dcdf72539f504d8bf922
Kaleab Alemu <kaleab.alemu@zendesk.com>
2021-10-12 13:20:54 -0500

    Brush up frontend and navigation

--- Frontend/baby_tracker_react/navigation/index.js
+++ Frontend/baby_tracker_react/navigation/index.js
@@ -3,16 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import welcome from '../screens/welcome';
 import SignInScreen from '../screens/signin';
-
+import options from '../screens/options'
 
 const stack = createNativeStackNavigator();
 const Navstack = () => {
     return (
        
-            <stack.Navigator  initialRouteName={welcome}>
+            <stack.Navigator  initialRouteName={welcome} >
 
                 <stack.Screen
-                name = "welcome" component={welcome}
+                name = "welcome" component={welcome} options={{headerShown: false}}
+                />
+                <stack.Screen
+                name="options" component={options} options={{headerShown: false}}
                 />
                 <stack.Screen
                 name = "login"
